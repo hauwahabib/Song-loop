9e3fc2b46b4eff3ab7665dbfd6e3b8f096a50123
hauwa.habib <108408200+hauwahabib@users.noreply.github.com>
2022-10-28 23:28:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Fill in the blank lyrics!")
+print("(Type in the blank lyrics and see if you are as cool as me.)")
+print()
+while True:
+   lyric = input("Send me your ")
+   if lyric == "location":
+     break 
+   else:
+     print("Nope. Try again.")
+     print("Hint: it's by Khalid..")
+print("Alright! You got it🥳.")
+print("Send me your location🎶")
